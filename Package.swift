6d52605943f141c0816d93f6ea649e35ab1cbb4e
theo-ahnlabio <theo@ahnlab.io>
2026-05-28 15:58:16 +0900

    fix: abcmpc 의존 → 0.1.18 (0.1.34 release 누락분)

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["ABCWaas"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ahnlabio/abc-mpc-ios-lib", from: "0.1.17")
+        .package(url: "https://github.com/ahnlabio/abc-mpc-ios-lib", from: "0.1.18")
     ],
     targets: [
         .target(
